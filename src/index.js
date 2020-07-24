9765e358672efcc68a85907a9282832fee08ad09
JD <jdpattonv@gmail.com>
2020-07-23 22:30:02 -0400

    'db'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import logger from 'loglevel'
-import {startServer} from './start'
+import logger from 'loglevel';
+import { startServer } from './start';
 
-const isTest = process.env.NODE_ENV === 'test'
-const logLevel = process.env.LOG_LEVEL || (isTest ? 'warn' : 'info')
+const isTest = process.env.NODE_ENV === 'test';
+const logLevel = process.env.LOG_LEVEL || (isTest ? 'warn' : 'info');
 
-logger.setLevel(logLevel)
+logger.setLevel(logLevel);
 
-startServer()
+startServer();
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,39 @@
-
+const Sequelize = require('sequelize');
 require('dotenv').config({ path: 'process.env' });
 
-const mysql=require('mysql');
-const connection=mysql.createConnection({
-  host:process.env.DB_HOST,
+const env = {
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DATABASE,
   port: process.env.DB_PORT,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+
+  pool: {
+    max: env.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle,
+  },
 });
 
-connection.connect(function(error){
-  if(!!error){
-    console.log(error);
-  }else{
-    console.log('Connected to database!:)');
-  }
-});  
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-module.exports = connection; 
+// Models/tables
+db.jobs = require('../models/job.model.js')(sequelize, Sequelize);
+// db.trackingData = require('../models/trackingData.model.js')(sequelize, Sequelize);
 
+module.exports = db;
